ff844a30a9d0a1bd70592436e07d2eea97928151
github-action-benchmark <github@users.noreply.github.com>
2026-08-26 06:15:27 +0000

    add Benchmark (cargo) benchmark result for 9d30145b85871fb7f7bf0fad071eababa51c5634

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787655123746,
+  "lastUpdate": 1787724927961,
   "repoUrl": "https://github.com/hseeberger/tellus",
   "entries": {
     "Benchmark": [
@@ -119,6 +119,66 @@ window.BENCHMARK_DATA = {
             "name": "fan_out/workers/16",
             "value": 3438672,
             "range": "± 9176",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "git@heikoseeberger.de",
+            "name": "Heiko Seeberger",
+            "username": "hseeberger"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9d30145b85871fb7f7bf0fad071eababa51c5634",
+          "message": "Merge pull request #43 from hseeberger/dependabot/github_actions/ci-minor-492e2c7bf6\n\nci(deps): bump taiki-e/install-action from 2.85.11 to 2.86.3 in the ci-minor group",
+          "timestamp": "2026-08-26T08:13:32+02:00",
+          "tree_id": "7594b66bfd7abf97a6fe7fcb7214547c43badda4",
+          "url": "https://github.com/hseeberger/tellus/commit/9d30145b85871fb7f7bf0fad071eababa51c5634"
+        },
+        "date": 1787724927319,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "flood/unbounded",
+            "value": 12856282,
+            "range": "± 325185",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "flood/bounded",
+            "value": 14845775,
+            "range": "± 60858",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong/pairs/1",
+            "value": 644967,
+            "range": "± 9962",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong/pairs/4",
+            "value": 804735,
+            "range": "± 31842",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fan_out/workers/4",
+            "value": 4946714,
+            "range": "± 163515",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fan_out/workers/16",
+            "value": 3572652,
+            "range": "± 64889",
             "unit": "ns/iter"
           }
         ]
