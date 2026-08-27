056093e6c3d1d60c94cca8b2b0e88d9277a08e72
github-action-benchmark <github@users.noreply.github.com>
2026-08-27 15:55:14 +0000

    add Core (cargo) benchmark result for 779ef2f79d25a8532753cbbb110a8b93ce8bcbb9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787845461336,
+  "lastUpdate": 1787846114383,
   "repoUrl": "https://github.com/hseeberger/tellus",
   "entries": {
     "Core": [
@@ -239,6 +239,66 @@ window.BENCHMARK_DATA = {
             "name": "fan_out/workers/16",
             "value": 8874525,
             "range": "± 228582",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "git@heikoseeberger.de",
+            "name": "Heiko Seeberger",
+            "username": "hseeberger"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "779ef2f79d25a8532753cbbb110a8b93ce8bcbb9",
+          "message": "Merge pull request #52 from hseeberger/build/independent-crate-versions\n\nbuild: use independent versions per crate",
+          "timestamp": "2026-08-27T17:53:17+02:00",
+          "tree_id": "bb4b2115dc6995dd7741bc79ba515d70bc94d278",
+          "url": "https://github.com/hseeberger/tellus/commit/779ef2f79d25a8532753cbbb110a8b93ce8bcbb9"
+        },
+        "date": 1787846112870,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "flood/unbounded",
+            "value": 9100745,
+            "range": "± 111013",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "flood/bounded",
+            "value": 11188195,
+            "range": "± 103266",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong/pairs/1",
+            "value": 562944,
+            "range": "± 4973",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong/pairs/4",
+            "value": 613272,
+            "range": "± 3294",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fan_out/workers/4",
+            "value": 4339785,
+            "range": "± 30941",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fan_out/workers/16",
+            "value": 2779179,
+            "range": "± 10062",
             "unit": "ns/iter"
           }
         ]
