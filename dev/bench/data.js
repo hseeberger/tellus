008f3b3627ce76a97dcc91db56f4e277960de128
github-action-benchmark <github@users.noreply.github.com>
2026-09-08 19:30:45 +0000

    add Persistence (cargo) benchmark result for f3996e8d5447cd1961ddcafa7881a7a1ea606af7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788895843916,
+  "lastUpdate": 1788895845655,
   "repoUrl": "https://github.com/hseeberger/tellus",
   "entries": {
     "Core": [
@@ -1681,6 +1681,54 @@ window.BENCHMARK_DATA = {
             "name": "recover/snapshot",
             "value": 132039,
             "range": "± 5776",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "git@heikoseeberger.de",
+            "name": "Heiko Seeberger",
+            "username": "hseeberger"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3996e8d5447cd1961ddcafa7881a7a1ea606af7",
+          "message": "Merge pull request #68 from hseeberger/dependabot/cargo/cargo-minor-8d2c85db1c\n\nbuild(deps): bump uuid from 1.25.0 to 1.26.0 in the cargo-minor group",
+          "timestamp": "2026-09-08T21:26:44+02:00",
+          "tree_id": "11572b6facd660d429236b72c801d2fd9c4f8f16",
+          "url": "https://github.com/hseeberger/tellus/commit/f3996e8d5447cd1961ddcafa7881a7a1ea606af7"
+        },
+        "date": 1788895845348,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "persist/no_snapshots",
+            "value": 3106725,
+            "range": "± 18904",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "persist/snapshots",
+            "value": 3036835,
+            "range": "± 24106",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "recover/replay",
+            "value": 1620552,
+            "range": "± 3687",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "recover/snapshot",
+            "value": 205842,
+            "range": "± 3787",
             "unit": "ns/iter"
           }
         ]
